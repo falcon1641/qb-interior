be4af771a33d8c53e30ccdf85dd70b9f315bb02f
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-17 16:11:05 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,69 +12,69 @@ client_scripts {
 }
 
 files {
+	-- Default (included)
 	'stream/defaultshells/shellprops.ytyp',
 	'stream/startingapt/shellpropsv11.ytyp',
+    -- Warehouse Shells (addon) https://k4mb1.tebex.io/package/4673185
+    'stream/WarehouseShells/shellpropsv5.ytyp',
+    -- Highend Shells (addon) https://k4mb1.tebex.io/package/4673131
+	'stream/HighendHousingShells/shellpropsv9.ytyp',
+	-- Deluxe Shells (addon) https://k4mb1.tebex.io/package/4673159
+    'stream/DeluxeHousingShells/shellpropsv2.ytyp',
+    -- Office Shells (addon) https://k4mb1.tebex.io/package/4673258
+	'stream/OfficeShells/shellpropsv3.ytyp',
+    -- Store Shells (addon) https://k4mb1.tebex.io/package/4673264
+	'stream/StoreShells/shellpropsv4.ytyp',
+    -- Medium Housing (addon) https://k4mb1.tebex.io/package/4672307
+    'stream/MediumHousingShells/shellpropsv10.ytyp',
+	-- Drug Lab Shells (addon) https://k4mb1.tebex.io/package/4672285
+	'stream/LabShells/shellpropsv7.ytyp',
+    -- Garage Shells (addon) https://k4mb1.tebex.io/package/4673177
+	'stream/GarageShells/shellpropsv8.ytyp',
 	-- 'stream/ClassicHouseShells/shellpropsv19.ytyp',
-	-- 'stream/DeluxeHousingShells/shellpropsv2.ytyp',
 	-- 'stream/FurnishedHousingShells/shellpropsv11.ytyp',
 	-- 'stream/FurnishedMotelsShells/shellpropsv18.ytyp',
 	-- 'stream/FurnishedStashhousesShells/shellpropsv15.ytyp',
-	-- 'stream/GarageShells/shellpropsv8.ytyp',
-	-- 'stream/HighendHousingShells/shellpropsv9.ytyp',
 	-- 'stream/HighendLabShells/kshellsdrug.ytyp',
-	-- 'stream/LabShells/shellpropsv7.ytyp',
-	-- 'stream/MediumHousingShells/shellpropsv10.ytyp',
 	-- 'stream/ModernHotelsShells/shellpropsv14.ytyp',
 	-- 'stream/ModernHousingShells/shellpropsv12.ytyp',
-	-- 'stream/OfficeShells/shellpropsv3.ytyp',
-	-- 'stream/StashhousesShells/shellpropsv16.ytyp',
-	-- 'stream/StoreShells/shellpropsv4.ytyp',
-	-- 'stream/WarehouseShells/shellpropsv5.ytyp'
-	-- 'stream/AllShellsBundle/allshellsbundle.ytyp'
-	-- 'stream/AllFurnishedShellsBundle/allfurnishedshellsbundle.ytyp'
-	-- 'stream/AllEmptyShellsBundle/allemptyshellsbundle.ytyp'
+	-- 'stream/StashhousesShells/shellpropsv16.ytyp'
 }
 
 -- Default (included)
 data_file 'DLC_ITYP_REQUEST' 'stream/defaultshells/shellprops.ytyp'
 data_file 'DLC_ITYP_REQUEST' 'stream/startingapt/shellpropsv11.ytyp'
--- Classic
+-- Warehouse Shells (addon) https://k4mb1.tebex.io/package/4673185
+data_file 'DLC_ITYP_REQUEST' 'stream/WarehouseShells/shellpropsv5.ytyp'
+-- Highend Shells (addon) https://k4mb1.tebex.io/package/4673131
+data_file 'DLC_ITYP_REQUEST' 'stream/HighendHousingShells/shellpropsv9.ytyp'
+-- Deluxe Shells (addon) https://k4mb1.tebex.io/package/4673159
+data_file 'DLC_ITYP_REQUEST' 'stream/DeluxeHousingShells/shellpropsv2.ytyp'
+-- Office Shells (addon) https://k4mb1.tebex.io/package/4673258
+data_file 'DLC_ITYP_REQUEST' 'stream/OfficeShells/shellpropsv3.ytyp'
+-- Store Shells (addon) https://k4mb1.tebex.io/package/4673264
+data_file 'DLC_ITYP_REQUEST' 'stream/StoreShells/shellpropsv4.ytyp'
+-- Medium Housing (addon) https://k4mb1.tebex.io/package/4672307
+data_file 'DLC_ITYP_REQUEST' 'stream/MediumHousingShells/shellpropsv10.ytyp'
+-- Drug Lab Shells (addon) https://k4mb1.tebex.io/package/4672285
+data_file 'DLC_ITYP_REQUEST' 'stream/LabShells/shellpropsv7.ytyp'
+-- Garage Shells (addon) https://k4mb1.tebex.io/package/4673177
+data_file 'DLC_ITYP_REQUEST' 'stream/GarageShells/shellpropsv8.ytyp'
+-- -- Classic
 -- data_file 'DLC_ITYP_REQUEST' 'stream/ClassicHouseShells/shellpropsv19.ytyp'
--- -- Deluxe
--- data_file 'DLC_ITYP_REQUEST' 'stream/DeluxeHousingShells/shellpropsv2.ytyp'
 -- -- Furnished Housing
 -- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedHousingShells/shellpropsv11.ytyp'
 -- -- Furnished Motels
 -- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedMotelsShells/shellpropsv18.ytyp'
 -- -- Furnished Stash
 -- data_file 'DLC_ITYP_REQUEST' 'stream/FurnishedStashhousesShells/shellpropsv15.ytyp'
--- -- Garage
--- data_file 'DLC_ITYP_REQUEST' 'stream/GarageShells/shellpropsv8.ytyp'
--- -- High End
--- data_file 'DLC_ITYP_REQUEST' 'stream/HighendHousingShells/shellpropsv9.ytyp'
 -- -- High End Labs
 -- data_file 'DLC_ITYP_REQUEST' 'stream/HighendLabShells/kshellsdrug.ytyp'
--- -- Labs
--- data_file 'DLC_ITYP_REQUEST' 'stream/LabShells/shellpropsv7.ytyp'
--- -- Medium Housing
--- data_file 'DLC_ITYP_REQUEST' 'stream/MediumHousingShells/shellpropsv10.ytyp'
 -- -- Modern Hotels
 -- data_file 'DLC_ITYP_REQUEST' 'stream/ModernHotelsShells/shellpropsv14.ytyp'
 -- -- Modern Housing
 -- data_file 'DLC_ITYP_REQUEST' 'stream/ModernHousingShells/shellpropsv12.ytyp'
--- -- Office
--- data_file 'DLC_ITYP_REQUEST' 'stream/OfficeShells/shellpropsv3.ytyp'
 -- -- Stash
 -- data_file 'DLC_ITYP_REQUEST' 'stream/StashhousesShells/shellpropsv16.ytyp'
--- -- Store
--- data_file 'DLC_ITYP_REQUEST' 'stream/StoreShells/shellpropsv4.ytyp'
--- -- Warehouse
--- data_file 'DLC_ITYP_REQUEST' 'stream/WarehouseShells/shellpropsv5.ytyp'
--- -- All Shells Bundle
--- data_file 'DLC_ITYP_REQUEST' 'stream/AllShellsBundle/allshellsbundle.ytyp'
--- -- All Furnished Bundle
--- data_file 'DLC_ITYP_REQUEST' 'stream/AllFurnishedShellsBundle/allfurnishedshellsbundle.ytyp'
--- -- All Empty Bundle
--- data_file 'DLC_ITYP_REQUEST' 'stream/AllEmptyShellsBundle/allemptyshellsbundle.ytyp'
 
 lua54 'yes'
